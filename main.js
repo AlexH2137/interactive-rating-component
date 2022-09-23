3fd3d81a1750d04d4a51708c2298db65d383e0b9
AlexH2137 <109902844+AlexH2137@users.noreply.github.com>
2022-09-23 08:08:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,9 @@ let rating = document.querySelectorAll('.numbers');
 const form = document.querySelector('form')
 
 form.addEventListener('submit', e => {
-  const formData = new FormData(e.target); 
-  const formDataObject = Object.fromEntries(formData);
-  selected.innerText = formDataObject.rating;
-})
-
-submit.addEventListener('click', function(e) {
+    const formData = new FormData(e.target); 
+    const formDataObject = Object.fromEntries(formData);
+    selected.innerText = formDataObject.rating;
     e.preventDefault();
     submitContainer.style.display = 'flex';
     container.style.display = 'none'; 
